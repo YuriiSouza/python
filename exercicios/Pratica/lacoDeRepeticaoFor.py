0f9bc77f8cd8750db07a153582dcc469b09944c5
Yuri <yuripeixoto1112@gmail.com>
2022-01-07 23:45:55 -0300

    Estudos
    07/01/2022

---
+++ exercicios/Pratica/lacoDeRepeticaoFor.py
@@ -0,0 +1,8 @@
+print("Vamos contar de")
+x = int(input())
+print(" até ")
+y = int(input())
+print("\n")
+for n in range(x, y+1):
+    print(n)
+        
---
+++ exercicios/Pratica/mediaValoresWhile.py
@@ -0,0 +1,6 @@
+v = 0;
+soma = 0;
+media = 0
+while v >= 0:
+    v = float(input("Digite o valor: "));
+    soma = soma + v;
