3edf3ce782d6c8e8ca4a0760a0efd454d28152ee
Yuri <yuripeixoto1112@gmail.com>
2022-01-17 22:34:00 -0300

    .

--- exercicios/desafios/D024.py
+++ exercicios/desafios/D024.py
@@ -1,1 +1,8 @@
 #Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "santo".
+
+n = str(input("Digite um nome: "))
+
+nomeRepartido = n.split()
+
+print("O nome recebe com 'santo'? ")
+print(nomeRepartido[0] == 'santo')
--- exercicios/desafios/D025.py
+++ exercicios/desafios/D025.py
@@ -1,1 +1,6 @@
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem "silva" no nome.
+
+n = str(input("Digite um nome: "))
+
+print("Esse nome tem o sobrenome SILVA? ")
+print('silva' in n)
