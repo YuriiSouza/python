00e7304399574ca1b28498985af50ad37c0bbcbb
Yuri <yuripeixoto1112@gmail.com>
2022-02-14 23:40:17 -0300

    0

--- exercicios/desafios/D055.py
+++ exercicios/desafios/D055.py
@@ -1,1 +1,14 @@
 #Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+for i in range(0, 5):
+    p = int(input(print("Digíte o seu peso: ")))
+    
+    if i == 0:
+        maior = p
+        menor = p
+    elif p > maior:
+        maior = p
+    elif p < menor:
+        menor = p
+
+print(f"""O maior peso é {maior}
+O menor valor é {menor}""")
--- exercicios/desafios/D056.py
+++ exercicios/desafios/D056.py
@@ -1,1 +1,32 @@
 #Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
+mulheresmenosde20 = 0
+for i in range(0,4):
+    while True:
+        nome = str(input("Nome: "))
+        if (nome != '') and (type(nome) == str):
+            break
+    
+    idade = int(input("Idade: "))
+
+    while True:
+        sexo = str(input("Sexo: ")).lower()
+        if (sexo != '') and (type(sexo) == str) and (sexo == 'masculino' or sexo == 'feminino'):
+            break
+
+    idadetotal =+ int(idade);
+    
+    if (i == 0) and (sexo == 'masculino'):
+        maisvelho = nome;
+        maisidademasculino = idade;
+    elif (sexo == 'masculino') and (idade > maisidademasculino):
+        maisvelho = nome;
+        maisidademasculino = idade;
+
+    if (sexo == 'feminino') and (idade < 20):
+        mulheresmenosde20 = mulheresmenosde20 + 1;
+
+mediaidade = idadetotal/4
+
+print(f'''A média de idade do grupo é {mediaidade}
+O nome do homen mais velho é {maisvelho}
+Existem {mulheresmenosde20} com menos de 20 anos. ''')
