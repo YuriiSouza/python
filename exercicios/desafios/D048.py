df882a4734c98f535e5f64d5ad0697be2835712f
Yuri <yuripeixoto1112@gmail.com>
2022-02-19 10:17:50 -0300

    3

--- exercicios/desafios/D048.py
+++ exercicios/desafios/D048.py
@@ -1,9 +1,8 @@
 #Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo de 1 até 500.
 soma = 0
-for c in range(1,500):
-    if c % 3:
-        print("Somando: ")
-        print(c)
+for c in range(1,501):
+    if c % 3 == 0:
         soma = soma + c;
 
-print(f"A soma entre de todos os multiplosde 3 entre 1 e 500 é -> {soma}")
+print(" ")
+print(f"A soma entre de todos os multiplos de 3 entre 1 e 500 é -> {soma}")
