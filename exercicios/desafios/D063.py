da4f7ffdccb75b78f3da5181fe598243c4d5e29a
Yuri <yuripeixoto1112@gmail.com>
2022-03-03 23:20:04 -0300

    .

--- exercicios/desafios/D063.py
+++ exercicios/desafios/D063.py
@@ -1,1 +1,22 @@
 # Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros elementos de uma Sequência de Fibonacci. 
+n = int(input("Digite a quantidades de termos da sequência: "))
+
+cont = 0
+while cont < n:
+    if cont == 0:
+        f = cont
+        print(f)
+        cont += 1
+    elif cont == 1:
+        f = cont
+        print(f)
+        cont_1 = f
+        cont_2 = 0
+        cont += 1
+    else:
+        f = cont_1 + cont_2
+        print(f)
+        cont_2 = cont_1
+        cont_1 = f
+        cont += 1
+        
--- exercicios/desafios/D065.py
+++ exercicios/desafios/D065.py
@@ -1,1 +1,23 @@
 # Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+soma = 0
+cont = 0
+maior = 0
+r = ""
+while True:
+    n = int(input(":::"))
+    soma += n
+    cont += 1
+    if n > maior:
+        maior = n
+    if r == '':
+        menor = n
+    elif n < menor:
+        menor = n
+    r = input("Deseja continuar? ")
+    if r in 'Nn':
+        break
+
+media = soma/cont
+print(media)
+print(maior)
+print(menor)
