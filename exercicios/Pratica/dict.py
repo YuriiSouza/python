26b594242369c764303a6fceaf62ddd95fda9687
Yuri <yuripeixoto1112@gmail.com>
2022-03-16 07:15:33 -0300

    b

---
+++ exercicios/Pratica/dict.py
@@ -0,0 +1,9 @@
+dados = dict( 'nome': João, 'idade': 23)
+#dados = {'nome':'Joao', 'idade': 23}
+
+print(dados)
+
+#adicionando um indice com um valor ao dicionário
+dados["sexo"] = "m"
+
+print(dados)
