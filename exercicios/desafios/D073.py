072e9805dff210fa9809f532753d363acb287096
Yuri <yuripeixoto1112@gmail.com>
2022-03-17 23:05:44 -0300

    b

--- exercicios/desafios/D073.py
+++ exercicios/desafios/D073.py
@@ -3,3 +3,16 @@
 #b) Os últimos 4 colocados.
 #c) Times em ordem alfabética. 
 #d) Em que posição está o time da Chapecoense.
+times = ("Atlético", "Flamengo", "Palmeiras","Fortaleza","Corinthians","Bragantino","Fluminense","América","Atlético","Santos","Ceará","Internacional","São Paulo","Athletico","Cuiabá","Juventude","Grêmio","Bahia","Sport","Chapecoense")
+
+print("=-"*15)
+print("PRIMEIROS COLOCADOS")
+print("=-"*15)
+for n in range(0,5):
+    print(f"{n+1}º Lugar = {times[n]}")
+
+print("=-"*15)
+print("ULTIMOS COLOCADOS")
+print("=-"*15)
+for n, t in enumarete(times):
+     print((f"{len(times)-n}º lugar = {times[len(times)-n]}"))
