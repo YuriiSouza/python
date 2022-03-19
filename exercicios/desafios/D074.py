1d8043c5ae0d409b59825f0bf7f20c57f19a6c73
Yuri <yuripeixoto1112@gmail.com>
2022-03-19 20:23:57 -0300

    exercicio
    exercicio 74 e 74 do curso de python

--- exercicios/desafios/D074.py
+++ exercicios/desafios/D074.py
@@ -1,4 +1,18 @@
 #Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
-for n in range(0,5):
-    y = int(input(f"Digite o valor {n}: "))
-    
+from random import randint
+
+
+tupla = randint(0, 9999), randint(0, 9999), randint(0, 9999), randint(0, 9999), randint(0, 9999)
+
+for n in range(0, 5):
+    if n == 0:
+        menor = tupla[0]
+        maior = tupla[0]
+    elif menor > tupla[n]:
+        menor = tupla[n]
+    elif maior < tupla[n]:
+        maior = tupla[n]
+
+print(f"Entre {tupla}")
+print(f"MAIOR ==> {maior}")
+print(f"MENOR ==> {menor}")
--- exercicios/desafios/D075.py
+++ exercicios/desafios/D075.py
@@ -3,3 +3,31 @@
 #A) Quantas vezes apareceu o valor 9.
 #B) Em que posição foi digitado o primeiro valor 3.
 #C) Quais foram os números pares.
+
+tupla = int(input("Valor: ")), int(input("Valor: ")), int(input("Valor: ")), int(input("Valor: "))
+
+c = cont9 = 0
+while c < 4:
+    if tupla[c] == 9:
+        cont9 += 1
+    c += 1
+
+if cont9 > 0:
+    print(f"Apareceram {cont9} noves...")
+else:
+    print("Não apareceu nove...")
+
+if tupla.index(3) >= 0:
+    print(f"O 3 foi o {tupla.index(3) + 1} número a ser digitado.")
+else:
+    print("O valor 3 não foi digitado.")
+
+print("Os números pares: ")
+
+c = quantPar =  0
+for c in range(0, 4):
+    if tupla[c] % 2 == 0:
+        print(tupla[c], end = ', ')
+        quantPar += 1
+if quantPar == 0:
+    print(f"Não foram digitados números pares.")
