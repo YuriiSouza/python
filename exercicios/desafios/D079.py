4098108e6a3d337cafe2cf1f491e769b61ab351a
Yuri <yuripeixoto1112@gmail.com>
2022-03-20 13:40:53 -0300

    c

--- exercicios/desafios/D079.py
+++ exercicios/desafios/D079.py
@@ -1,5 +1,8 @@
 #Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
-valores = [list]
+from statistics import median_grouped
+
+
+valores = list()
 
 while True:
     valor = int(input("Valor: "))
@@ -15,10 +18,10 @@ while True:
     if continua in 'Nn':
         break
 
-cont = menor = cont1 = 0
-valoresOrdenado = [list]
+#cont = menor = cont1 = 0
+valoresOrdenado = list()
 
-while cont < len(valores):
+'''while cont < len(valores):
     while cont1 < len(valores):
         if cont1 == 0:
             menor == valores[cont1]
@@ -27,6 +30,15 @@ while cont < len(valores):
         cont1 = cont1 + 1
     valores.append(menor)
     cont = cont + 1
-
+'''
+menor = 0
+for cont in range(0, len(valores)):
+    for cont1 in range(0, len(valores)):
+        if cont1 == 0:
+            menor = valores[cont1]
+        elif menor > valores[cont1]:
+            menor = valores[cont1]
+    valoresOrdenado.append(menor)
+    valores.remove(menor)
 print(valores)
 print(valoresOrdenado)
--- exercicios/desafios/D080.py
+++ exercicios/desafios/D080.py
@@ -1,1 +1,28 @@
 #Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
+valores = list()
+
+maior = 0
+for n in range(0, 5):
+    valor = int(input(f"Valor {n}: "))
+    if n == 0:
+        valores.append(valor)
+    '''for cont in range(len[valores], -1, -1):
+        if valor > valores[len(valores)-cont]:
+            valores.append(valor)
+        elif valores[len(valores)-cont] > valor:
+            maior = valores[len(valores)-cont]
+            del valores[len(valores)-cont]
+            valores.append(valor)
+            valores.append(maior)'''
+    
+
+    if valor > valores[len(valores)-1]:
+        valores.append(valor)
+    for cont in range(len(valores)-1, -1, -1):
+        if valores[cont] > valor:
+            maior = valores[cont]
+            del valores[cont]
+            valores.append(valor)
+            valores.append(maior)
+
+print(valores)
