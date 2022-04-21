3a2fb5ec65527e10d226c399b336b977a7a887b2
Yuri <yuripeixoto1112@gmail.com>
2022-04-21 17:06:33 -0300

    ;

--- exercicios/desafios/D085.py
+++ exercicios/desafios/D085.py
@@ -1,4 +1,19 @@
 #Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 
-generalList = list()
-#In this list,
+x = y = 0
+generalListodd = list()
+generalListpair = list()
+
+#In this list, i will append all the values, all separate in a matrix
+
+for n in range(0,7):
+    number = int(input("Value = "))
+    
+    if number % 2 == 0:
+        generalListodd.append(number)
+    else:
+        generalListpair.append(number)
+
+
+print(f"odd = {generalListodd}")
+print(f"pair = {generalListpair}")
--- exercicios/desafios/D086.py
+++ exercicios/desafios/D086.py
@@ -1,1 +1,10 @@
 # Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
+matrix = ([],[],[])
+
+for line in range(0, 3):
+    for column in range(0, 3):
+        number = int(input(f"Number of line {line + 1} and column {column + 1}: "))
+        matrix[line][column].append(number)
+
+for n in range(0, 3):
+    print(matrix[n])
