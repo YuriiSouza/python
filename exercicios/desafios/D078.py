2e9ce27e5083c285630c79b3245376080e673dc1
Yuri <yuripeixoto1112@gmail.com>
2022-05-05 20:11:26 -0300

    v

--- exercicios/desafios/D078.py
+++ exercicios/desafios/D078.py
@@ -1,27 +1,22 @@
 # Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista. 
 listaNumerica = []
-listmaior = []
-listmenor = []
 
 for n in range(0, 5):
     j = int(input("Digite o valor: "))
     listaNumerica.append(j)
     
-for n in listaNumerica:
-    if n == max(listaNumerica):
-        listmaior.append(listaNumerica.index)
-    if n == min(listaNumerica):
-        listmenor.append(listaNumerica.index)
-
 print("")
 
 print("VALORES", end = ":::")
 print(listaNumerica)
 
 print(f"O valor menor {min(listaNumerica)} esta nas posições: ", end = "")
-for n, i in enumerate(listmenor):
-    print(i, "...", end = " ")
+for n, i in enumerate(listaNumerica):
+    if i == min(listaNumerica):
+        print(f"{n + 1} ...", end = "...")
 
-print(f"O valor maior {max(listaNumerica)} está nas posições: ")
-for n in range(0, len(listmaior)):
-    print(f"{n}...", end = " ")
+print(f"\nO valor maior {max(listaNumerica)} está nas posições: ", end = "")
+
+for n, i in enumerate(listaNumerica):
+    if i == max(listaNumerica):
+        print(f"{n + 1} ..." , end = "")
