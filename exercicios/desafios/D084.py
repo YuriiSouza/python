75f29094770c377855e16ca332a31962d0c04342
Yuri <yuripeixoto1112@gmail.com>
2022-05-07 17:14:51 -0300

    m

--- exercicios/desafios/D084.py
+++ exercicios/desafios/D084.py
@@ -3,23 +3,22 @@
 #B) Uma listagem com as pessoas mais pesadas.
 #C) Uma listagem com as pessoas mais leves.
 
-from this import d
-
-
 List = []
 princ = []
-mai = men = 0
+mai = 0
+men = 0
 
 while True:
     List.append(str(input("Name: ")))
     List.append(int(input("Weight: ")))
     if len(princ) == 0:
-        mai = men = 0
+        mai = men = List[1]
     else:
+        if men > List[1]:
+            men = List[1]
         if mai < List[1]:
             mai = List[1]
-        elif men > List[1]:
-            men = List[1]
+        
 
     princ.append(List[:])
     List.clear
@@ -30,7 +29,13 @@ while True:
 
 
 print("")
-print(f"Number of peoples inserts is {len(princ) + 1}\n")
+print(f"Number of peoples inserts is {len(princ)}\n")
 
 print(f"O wegihter is {mai}")
+for n in princ:
+    if n[1] == mai:
+        print(n[0])
 print(f"The ligthest is {men}")
+for n in princ:
+    if n[1] == men:
+        print(n[0])
