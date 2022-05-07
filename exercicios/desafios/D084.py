da5f729c272b371f74e2f33a4d9f47ad5960b2ac
Yuri <yuripeixoto1112@gmail.com>
2022-05-07 13:37:37 -0300

    h

--- exercicios/desafios/D084.py
+++ exercicios/desafios/D084.py
@@ -2,36 +2,27 @@
 #A) Quantas pessoas foram cadastradas.
 #B) Uma listagem com as pessoas mais pesadas.
 #C) Uma listagem com as pessoas mais leves.
-names = list()
-weight = list()
-mostWeight = list()
-lighter = list()
-mostWeightName = list()
-lighterName = list()
+
+List = []
+princ = []
 
 while True:
-    name = str(input("Insert name: "))
-    weight1 = int(input("Insert the weight: "))
-    names.append(name)
-    weight.append(weight1)
+    List.append(str(input("Name: ")))
+    List.append(int(input("Weight: ")))
+    princ.append(List[:])
+    List.clear
     
-    if weight1 > 70:
-        mostWeightName.append(name)
-        mostWeight.append(weight1)
-    else:
-        lighterName.append(name)
-        lighter.append(weight1)
-
-    verification = str(input("Do you wanna continue[Y/N]: "))
-    if verification in 'Nn':
-        break;
+    validation = str(input("Do you wanna continue? "))
+    if validation in 'Nn':
+        break
 
 
 print("")
-print(f"Number of peoples inserts is {len(names)}\n")
+print(f"Number of peoples inserts is {len(princ)}\n")
 print(f"\nThe ligthers is the:")
-for n in range(0, len(lighterName)):
-    print(lighterName[n], end = ' ')
+for n in range(0, len(princ)):
+    if princ[n][2] < 70:
+        print(princ[n][], end = ' ')
     print(f"The  weight", lighter[n])
 print(f"\nThe fatters is: ")
 for n in range(0, len(mostWeightName)):
