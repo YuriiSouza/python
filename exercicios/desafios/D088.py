155237a449f9b552c368aa962dbd24002d314ba0
Yuri <yuripeixoto1112@gmail.com>
2022-05-14 01:21:44 -0300

    7

--- exercicios/desafios/D088.py
+++ exercicios/desafios/D088.py
@@ -1,19 +1,24 @@
 #Faça um programa que ajude um jogador da MEGA SENA a criar palpites.O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
 from random import randint
 
+number = cont = 0
+tabela = list()
+todasTabelas = list()
 
-tabela = [[], []]
 n = int(input("How many games you want? "))
 
 for x in range(0, n):
-    for l in range(0, 2):
-        for c in range(0, 3):
-            tabela[l].append(randint(0, 61))
-
-    for l in range(0, 2):
-        print(" ")
-        for c in range(0, 3):
-            print(tabela[l][c], end = " ")
-    print(" ")
-    print("=-"*20)
+    tabela.clear()
+    cont = 0
+    while True:
+        number = randint(1, 60)
+        if number not in tabela:
+            tabela.append(number)
+            cont = cont + 1;
+        if cont >= 6:
+            break;
     
+    todasTabelas.append(tabela[:])
+
+for x in range(0, len(todasTabelas)):
+    print(todasTabelas[x])
--- exercicios/desafios/D089.py
+++ exercicios/desafios/D089.py
@@ -1,2 +1,50 @@
 #Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.
+from unicodedata import name
 
+
+list1 = list()
+allList = list()
+
+while True:
+    list1.clear()
+    list1.append(str(input("Name: ")).lower())
+    list1.append(float(input("Resulted 1: ")))
+    list1.append(float(input("Resulted 2: ")))
+
+    allList.append(list1[:])
+
+    question = str(input("Do you continue?\n ")).lower()
+    if question in 'no':
+        break
+
+for x in range(0, len(allList)):
+    print("")
+    print("=-"*10)
+    print(f"Name: {allList[x][0]}")
+    print(f"1 -> {allList[x][1]}")
+    print(f"2 -> {allList[x][2]}")
+    print("=-"*10)
+    print("")
+
+verification = str(input("Do you wanna see the results of someone? ")).lower()
+if verification in 'y':
+    while True:    
+        name = str(input("Name: "))
+        for x in range(0, len(allList)):
+            if allList[x][0] == name:
+                media = (allList[x][1] + allList[x][2]) / 2
+                print("")
+                print("=-"*10)
+                print(f"Name: {allList[x][0]}")
+                print(f"1 -> {allList[x][1]}")
+                print(f"2 -> {allList[x][2]}")
+                print(f"The MEDIA: {media}")
+                print("=-"*10)
+                print("")
+            
+    verify = str(input("Do you wanna see the results of someone? ")).lower()
+    if verify in 'no':
+        break
+
+else:
+    print("GOOD BYE")
