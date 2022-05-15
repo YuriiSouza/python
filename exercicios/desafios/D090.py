1f690fafa5cedbdacee2df0e2dc865c84432cd0d
Yuri <yuripeixoto1112@gmail.com>
2022-05-15 11:56:26 -0300

    j

--- exercicios/desafios/D090.py
+++ exercicios/desafios/D090.py
@@ -1,7 +1,12 @@
 #Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.
 dados = dict()
 
-dados['nome'] = str(input("What's that name? "))
+dados['Name'] = str(input("What's that name? "))
 dados['Avarege'] = float(input("What's the avarage "))
+if dados['Avarege'] >= 6.0:
+    dados['Situation'] = 'Approved'
+else:
+    dados['Situation'] = 'Desapproved'
 
-print(dados)
+for x, y in dados.items():
+    print(f"{x} = {y}")
