e57e0f223a188c6972a1361fac03356696d026d4
Yuri <yuripeixoto1112@gmail.com>
2022-05-16 23:03:39 -0300

    bom

--- exercicios/desafios/D091.py
+++ exercicios/desafios/D091.py
@@ -1,1 +1,20 @@
 #Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário em Python. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
+
+from operator import itemgetter
+from random import randint
+from time import sleep
+
+dados = dict()
+dadosRanking = list()
+
+for n in range(0, 4):
+    dados[f'Player {n + 1}'] = randint(0, 9)
+for x, y in dados.items():
+    print(f"{x} = {y}")
+
+dadosRanking = sorted(dados.items(), key = itemgetter(1), reverse = True)
+print("-==RANKING OF PLAYER==-")
+
+for x, y in enumerate(dadosRanking):
+    print(f"{x + 1}º place goes to {y[0]}  with {y[1]} points")
+    sleep(1)
--- exercicios/desafios/D092.py
+++ exercicios/desafios/D092.py
@@ -1,1 +1,17 @@
 # Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade) em um dicionário. Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
+from datetime import date
+dados = dict()
+
+dados['nome'] = str(input('Name: '))
+year = int(input('Year of birth: '))
+dados["years"] = date.today().year - year
+dados['work card'] = int(input("Number of work card: "))
+if dados['work card'] > 0:
+    dados['Year od contratation'] = int(input("Year od contratation: "))
+    dados['wage'] = ("R$") + str(input('Wage: '))
+    dados['retiriment'] = dados['years'] + (35 - (date.today().year - dados['Year od contratation']))
+
+print("=-" * 15)
+for n, y in dados.items():
+    print(f"{n} has the value {y}")
+    
