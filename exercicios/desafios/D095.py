abdf85a128c21d88d54bea956c285d5a42cf6fac
Yuri <yuripeixoto1112@gmail.com>
2022-05-20 23:23:29 -0300

    oimo

--- exercicios/desafios/D095.py
+++ exercicios/desafios/D095.py
@@ -1,13 +1,14 @@
 #Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
 total = 0
+cont = 1
 dados = dict()
 gols = list()
 time = list()
 
 while True:
     while True:
-        dados['nome'] = str(input("Qual o nome do jogador? "))
-        if int(dados["nome"]) == False:
+        dados['nome'] = str(input("Qual o nome do jogador? ")).lower()
+        if dados["nome"] == "":
             print("ERRO")
         else:
             break
@@ -50,3 +51,18 @@ for k, v in enumerate(time):
     for x in v.values():
         print(f'{str(x):<10}', end = "")
     print("")
+
+while True:
+    mais = str(input("Você quer saber os dados de alguem em especifico? ")).lower()[0]
+
+    if mais == 's':
+        nome = str(input("Nome: ")).lower()
+        for n in time:
+            if nome == n['nome']:
+                print(f'{n["nome"]:<5}')
+                for y in n["gols"]:
+                    print(f'{f"Gols {cont}º":<5}:{y}')
+                    cont += 1
+                print(f'{"Total":<5}{n["total"]}')
+    else:
+        break
