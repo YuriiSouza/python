b28518a4eda7bbd655bee1769abbc9d17d5bea47
Yuri <yuripeixoto1112@gmail.com>
2022-06-14 22:07:59 -0300

    N

--- exercicios/desafios/D106.py
+++ exercicios/desafios/D106.py
@@ -1,1 +1,17 @@
 #Faça um mini-sistema que utilize o Interactive Help do Python. O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra 'FIM', o programa se encerrará. Importante: use cores.
+def printa(msn):
+    n = (len(msn)+4)
+    print('=' * n)
+    print(f'  {msn}  ')
+    print("=" * n)
+
+
+while True:
+    fuc = str(input("Biblioteca ou função: "))
+
+    if fuc == 'fim':
+        break
+    
+    printa(f"The manual of the {fuc}")
+    print(help(fuc))
+    
