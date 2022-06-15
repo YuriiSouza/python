2c70f8596941975050521c83599f46ecef0a96be
Yuri <yuripeixoto1112@gmail.com>
2022-06-14 23:12:54 -0300

    b

---
+++ exercicios/modulos/teste1.py
@@ -0,0 +1,7 @@
+from socket import NI_NUMERICHOST
+from exercicios.modulos.uteis.factorial.__int__ import factorial
+import uteis
+num = int(input("NUMERO: "))
+
+fat = numeros.factorial(num)
+print(f"O fatorial de {num} é {fat}")
---
+++ exercicios/modulos/uteis/numeros/__int__.py
@@ -0,0 +1,13 @@
+def factorial(num):
+    f = 1
+    for n in range(1, num + 1):
+        f *= n
+
+
+def dobro(num):
+    f = num * 2
+
+
+def triplo(num):
+    f = num * 3
+
