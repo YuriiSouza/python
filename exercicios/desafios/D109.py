339e2906bec9fa3a1f7cbf260de6810c0a8d90d5
Yuri <yuripeixoto1112@gmail.com>
2022-06-17 18:13:11 -0300

    concertar o D110

--- exercicios/desafios/D109.py
+++ exercicios/desafios/D109.py
@@ -1,1 +1,3 @@
-#Modifique as fun√ß√µes que form criadas no desafio 107 para que elas aceitem um par√¢metro a mais, informando se o valor retornado por elas vai ser ou n√£o formatado pela fun√ß√£o moeda(), desenvolvida no desafio 108.
+#Modifique as funÁıes que form criadas no desafio 107 para que elas aceitem um par‚metro a mais, informando se o valor retornado por elas vai ser ou n„o formatado pela funÁ„o moeda(), desenvolvida no desafio 108.
+import moeda
+print(moeda.increase(100.3, 2, formatation = True))
--- exercicios/desafios/D110.py
+++
@@ -1,3 +0,0 @@
-#Modifique as funções que form criadas no desafio 107 para que elas aceitem um parâmetro a mais, informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
-import moeda
-print(moeda.increase(100.3, 2, formatation = True))
