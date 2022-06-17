23b73fd295ade69b789c9dffd37d6fbaa5b5bf85
Yuri <yuripeixoto1112@gmail.com>
2022-06-17 18:03:02 -0300

    .

--- exercicios/desafios/D108.py
+++ exercicios/desafios/D108.py
@@ -1,18 +1,25 @@
 # Adapte o código do desafio #107, criando uma função adicional chamada moeda() que consiga mostrar os números como um valor monetário formatado.
 import moeda
 
+form = str(input("Do you wanna the formatted mode?")).lower()[0]
 val = str(input("value: R$"))
 
 if ',' in val:
   val = float(val.replace(',', '.'))
-  double = moeda.double(val)
+  if form == 'y':
+    double = moeda.double(val, True)
+  else:
+    double = moeda.double(val)
   print(f"The double is R${double},00")
+  
 else:
   val = float(val)
-  double = moeda.double(val)
-  print(f"The double is R${double:0.2f}")
+  f form == 'y':
+    double = moeda.double(val, True)
+  else:
+    double = moeda.double(val)
+  print(f"The double is {double}")
 
 increased = moeda.increase(val, 1000)
 
-print(f"The value of R$1000 plus R${val} is", end = ' ')
-moeda.print_formatted(increased)
+print(f"The value of R$1000 plus R${val} is",moeda.formatted(increased))
