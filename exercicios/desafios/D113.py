25ed8fab67b21daa1236c67de900d2976c5bdbbe
Yuri <yuripeixoto1112@gmail.com>
2022-06-19 10:42:02 -0300

    .

--- exercicios/desafios/D113.py
+++ exercicios/desafios/D113.py
@@ -1,1 +1,10 @@
 # Reescreva a função leiaInt() que fizemos no desafio 104, incluindo agora a possibilidade da digitação de um número de tipo inválido. Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.
+def leiaint(mensagem):
+    while True:
+        n = input(mensagem)
+        if n.isnumeric():
+            break
+        else:
+            print("Valor não é inteiro")
+    
+    return n
