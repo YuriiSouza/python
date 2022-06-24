aaee9e12c03b840b88aaab5cd4a44662f2b6578b
Yuri <yuripeixoto1112@gmail.com>
2022-06-23 23:17:58 -0300

    Terminei
    Curso do Guanabara de python finalizado

--- exercicios/desafios/d115/modulo_ex115/__init__.py
+++ exercicios/desafios/d115/modulo_ex115/__init__.py
@@ -1,14 +1,11 @@
-from re import X
-
-
 def linha(tam = 30):
     print('='*tam)
 
 
-def cabecalho(msn):
-    linha()
-    print(msn.center(30))
-    linha()
+def cabecalho(msn, tam = 30):
+    linha(tam)
+    print(msn.center(tam))
+    linha(tam)
 
 
 def print_menu(lista):
@@ -52,4 +49,33 @@ def create_file(name):
         open(name, 'x')
     except:
         print("Status: File exist...")
+    else:
+        print("Status: File was create....")
+        
+
+def cadastrar(arq):
+    cabecalho('Novo registro')
+    try:
+        data = open(arq, 'a')
+    except:
+        print(f"Erro ao tentar abrir arquivo {arq}.")
+    else:
+        nome = str(input("Nome: "))
+        idade = int(input("Idade: "))
+        data.write(f'{nome};{idade}\n')
+
+    
+def mostra(arq):
+    print(f'{"Nome":<20}{"Idade":>9}')
+    linha()
+    try:
+        d = open(arq, 'r')
+    except:
+        print(f"Erro ao tentar abrir arquivo {arq}.")
+    else:
+        for n in d:
+            dados = n.split(';')
+            dados[1] = dados[1].replace('\n', '')
+            print(f'{dados[0].capitalize():<20}{dados[1]:>4} anos')
+        # print(d.read().replace(';', '\t\t').replace('\n', ' anos\n')) oq eu fiz sozinho
 
--- exercicios/desafios/d115/sistema.py
+++ exercicios/desafios/d115/sistema.py
@@ -1,24 +1,15 @@
 from modulo_ex115 import *
 
-
 create_file('data.txt')
-nome = list()
 
 while True:
     var_resp = print_menu(('Adcionar pessoa', 'Mostrar lista', 'Sair'))
     if var_resp == 1:#Adicionar
-        data = open('data.txt', 'a')
-        data.writelines('[')
-        data.writelines(str(input("Nome: ")))
-        data.writelines(' , ')
-        data.writelines(str(input("Idade: ")))
-        data.writelines(']')
+        cadastrar('data.txt')
+
     elif var_resp == 2:# Mostrar lista
-        data = open('data.txt', 'r')
-        nome = data.readlines()
-        for n in nome:
-            print(f'{n[0]}      {n[1]}')
+        mostra('data.txt')
+
     elif var_resp == 3:# Sair
-        print('3')
         cabecalho('FIM')
         break
