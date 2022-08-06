7e5a51be2a83d20b63ef500e2c001cf7712d331d
Yuri <yuripeixoto1112@gmail.com>
2022-08-06 00:29:48 -0300

    codar sempre q der mas nn aguento mais, preciso dormir

---
+++ practice_alone/kwargAndArgs/classes.py
@@ -0,0 +1,12 @@
+class tubarão:
+    def nadar(self):
+        print("O tubaralan está nadando...")
+
+
+    def morder(tubarão_com_fome, self):
+        if tubarão == True:
+            print('morde')
+        else:
+            print('não morde')
+
+            
---
+++ practice_alone/kwargAndArgs/teste.py
@@ -0,0 +1,7 @@
+def alguns_argumentos(arg_1, arg_2):
+    print(arg_1)
+    print(arg_2)
+
+
+args = ('Yuri', 'Souza')
+alguns_argumentos(*args)
