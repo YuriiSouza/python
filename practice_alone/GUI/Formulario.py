b2c18f39224c10797d7211c08fea2c7726bc2752
Yuri <yuripeixoto1112@gmail.com>
2022-08-20 13:26:15 -0300

    .

--- practice_alone/GUI/Formulario.py
+++ practice_alone/GUI/Formulario.py
@@ -2,14 +2,13 @@
 from tkinter import *
 from tkinter import ttk
 
-janela = Tk()
+janela = Tk()  
 
 janela.title("Formulário")
 janela.geometry('720x720')
 janela.configure(background= 'black')
 
 ttk.Button(janela, text = 'Bem vindo').grid()
-
 
 
 janela.mainloop()
