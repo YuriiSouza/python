383f8ee1e44f23d929c71b51cb3e12cc8ea08923
Yuri <yuripeixoto1112@gmail.com>
2022-08-20 09:47:42 -0300

    tkinter
    Lendo o documento da biblioteca para ver o que a ferramenta pode me oferecer

---
+++ practice_alone/GUI/Formulario.py
@@ -0,0 +1,15 @@
+'''Vamos aprender a criar uma GUI(graphical user interfaces)'''
+from tkinter import *
+from tkinter import ttk
+
+janela = Tk()
+
+janela.title("Formulário")
+janela.geometry('720x720')
+janela.configure(background= 'black')
+
+ttk.Button(janela, text = 'Bem vindo').grid()
+
+
+
+janela.mainloop()
