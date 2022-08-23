a1aa5f9336710338467be134228b546c13a2ead0
Yuri <yuripeixoto1112@gmail.com>
2022-08-22 23:00:53 -0300

    estudos tkinter

--- practice_alone/GUI/Formulario.py
+++ practice_alone/GUI/Formulario.py
@@ -1,19 +1,26 @@
 '''Vamos aprender a criar uma GUI(graphical user interfaces)'''
-from cProfile import label
-from tkinter import *
+''' icons' creator: https://www.flaticon.com/free-icons/flower'''
+import tkinter as tk
 from tkinter import ttk
-from tkinter import colorchooser
-import tkinter
 
-janela_principal = Tk()  
+def centralizar_window(window, window_widht, window_height):
+    centro_x = int(window.winfo_screenwidth() / 2) - int(window_widht / 2)
+    centro_y = int(window.winfo_screenheight() / 2) - int(window_height / 2)
 
+    window.geometry(f'{window_widht}x{window_height}+{centro_x}+{centro_y}')
+
+
+janela_principal = tk.Tk()  
 janela_principal.title("Formulário")
-janela_principal.geometry('720x720')
 
-mensagem_inicial = Label(janela_principal, text="formulario")
-mensagem_inicial.pack()
+centralizar_window(janela_principal, 720, 480)
+janela_principal.attributes('-alpha', 1)
 
-'''ttk.Button(janela_principal, text = 'Bem vindo').grid()'''
+janela_principal.iconbitmap('C:/Users/yurip/OneDrive/Documentos/Estudos/hum/python/practice_alone/GUI/Flor.ico')
+
+tk.Label(text = 'Label classico').pack()
+ttk.Label(text = 'Label temático').pack()
 
 
 janela_principal.mainloop()
+
