45ca9f0a76ffcfb2684553789f8fbb8c15accc0f
Yuri <yuripeixoto1112@gmail.com>
2022-08-23 22:22:26 -0300

    tentei

--- practice_alone/GUI/Formulario.py
+++ practice_alone/GUI/Formulario.py
@@ -10,17 +10,32 @@ def centralizar_window(window, window_widht, window_height):
     window.geometry(f'{window_widht}x{window_height}+{centro_x}+{centro_y}')
 
 
+def button_clicked():
+    print('"Click"')
+
+
+def select(opção):
+    print(opção)
+
+
+#configuração da borda da janela
 janela_principal = tk.Tk()  
 janela_principal.title("Formulário")
+janela_principal.iconbitmap('C:/Users/yurip/OneDrive/Documentos/Estudos/hum/python/practice_alone/GUI/Flor.ico')
 
+
+#configuração de dimensão e layout da janela
 centralizar_window(janela_principal, 720, 480)
 janela_principal.attributes('-alpha', 1)
 
-janela_principal.iconbitmap('C:/Users/yurip/OneDrive/Documentos/Estudos/hum/python/practice_alone/GUI/Flor.ico')
 
+#Layout dentro da janela
 tk.Label(text = 'Label classico').pack()
 ttk.Label(text = 'Label temático').pack()
 
+ttk.Button(janela_principal, text = 'cutuque', command=button_clicked).pack()
+ttk.Button(janela_principal, text = 'Sonhar', command=lambda : select('Sonhar')).pack()
+ttk.Button(janela_principal, text = 'Realizar', command=lambda : select('Realizar')).pack()
 
 janela_principal.mainloop()
 
